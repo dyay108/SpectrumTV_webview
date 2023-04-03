650f08d757ed5bdce197e32b3e99313a9b9a35d8
dyay108 <djboi94@gmail.com>
2023-04-02 21:43:24 -0500

    tv guide initial work

--- app/src/main/java/com/workaround/spectv/MainActivity.java
+++ app/src/main/java/com/workaround/spectv/MainActivity.java
@@ -2,7 +2,11 @@ package com.workaround.spectv;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.ConsoleMessage;
+import android.webkit.JavascriptInterface;
 import android.webkit.PermissionRequest;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
@@ -16,21 +20,27 @@ import androidx.fragment.app.FragmentActivity;
  */
 public class MainActivity extends FragmentActivity {
 
-    WebView myWebView;
+    final String uaString = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36";
+    final String guideUrl = "https://watch.spectrum.net/guide";
+    WebView spectrumPlayer;
+    WebView spectrumGuide;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myWebView = (WebView) findViewById(R.id.spectv);
-        myWebView.setWebChromeClient(new WebChromeClient() {
-//            @Override
-//            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
-//                Log.d("**************************", consoleMessage.message() + " -- From line " +
-//                        consoleMessage.lineNumber() + " of " + consoleMessage.sourceId());
-//                return true;
-//            }
+        spectrumPlayer = (WebView) findViewById(R.id.spectv);
+        spectrumGuide = (WebView) findViewById(R.id.spectv_guide);
+        spectrumGuide.setVisibility(View.GONE);
+
+        spectrumPlayer.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
+                Log.d("**************************", consoleMessage.message() + " -- From line " +
+                        consoleMessage.lineNumber() + " of " + consoleMessage.sourceId());
+                return true;
+            }
 
             @Override
             public void onPermissionRequest(PermissionRequest request) {
@@ -47,17 +57,20 @@ public class MainActivity extends FragmentActivity {
 
 
         });
-        WebSettings webSettings = myWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setDomStorageEnabled(true);
-        webSettings.setMediaPlaybackRequiresUserGesture(false);
-        webSettings.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW);
-        webSettings.setUserAgentString("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36");
-        myWebView.setWebViewClient(new WebViewClient() {
+        WebSettings spectrumPlayerWebSettings = spectrumPlayer.getSettings();
+        WebSettings spectrumGuideWebSettings = spectrumGuide.getSettings();
+
+        initWebviews(spectrumPlayerWebSettings);
+        initWebviews(spectrumGuideWebSettings);
+
+        spectrumGuide.addJavascriptInterface(this, "Specguide");
+        spectrumPlayer.addJavascriptInterface(this, "Spectv");
+
+        spectrumPlayer.setWebViewClient(new WebViewClient() {
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
-                myWebView.evaluateJavascript("var loopVar = setInterval(function() {" +
+                spectrumPlayer.evaluateJavascript("var loopVar = setInterval(function() {" +
                         "try{" +
                         // Accept initial prompts
                         "document.querySelector('[aria-label=\"Continue and accept terms and conditions to go to Spectrum TV\"]')?.click();" +
@@ -80,13 +93,16 @@ public class MainActivity extends FragmentActivity {
                         "catch(e){" +
                         "console.log(e)" +
                         "}" +
-                        "}, 2000);" , null);
+                        "}, 2000);" +
+                        "function channelSelected(s) {Spectv.changeChannel(s)}" +
+                        "$('[id^=\"channel-list-item\"]').on('click', (event) =>  console.log(\"&&&&&&&&&&&&&&&&&&&&&&\", event.target) )"
+                        , null);
 
             }
 
         });
-        myWebView.setVerticalScrollBarEnabled(false);
-        myWebView.loadUrl("https://watch.spectrum.net/?sessionOverride=true");
+        spectrumPlayer.setVerticalScrollBarEnabled(false);
+        spectrumPlayer.loadUrl("https://watch.spectrum.net/?sessionOverride=true");
     }
 
     @SuppressLint("RestrictedApi")
@@ -96,12 +112,38 @@ public class MainActivity extends FragmentActivity {
         if (event.getAction() == KeyEvent.ACTION_DOWN ) {
             if(event.getKeyCode() == KeyEvent.KEYCODE_DPAD_RIGHT || event.getKeyCode() == KeyEvent.KEYCODE_DPAD_LEFT) {
                 // Simulate clicking on the video player which brings up the mini channel guide (just like on desktop)
-                myWebView.evaluateJavascript("$('#spectrum-player').focus().click();", null);
+                spectrumPlayer.evaluateJavascript("$('#spectrum-player').focus().click();", null);
+
+
+                spectrumPlayer.evaluateJavascript("channelSelected('yooooooo');", null);
+
                 return true;
             }
         }
 
         return super.dispatchKeyEvent(event);
+    }
+
+    @JavascriptInterface
+    public void changeChannel(String test) {
+        switch (spectrumGuide.getVisibility() ) {
+            case View.GONE:
+                Log.d("***********showing ", test);
+                spectrumGuide.setVisibility(View.VISIBLE);
+                break;
+            case View.VISIBLE:
+                Log.d("***********hiding ", test);
+                spectrumGuide.setVisibility(View.GONE);
+                break;
+        }
+    }
+
+    private void initWebviews(WebSettings wv) {
+        wv.setJavaScriptEnabled(true);
+        wv.setDomStorageEnabled(true);
+        wv.setMediaPlaybackRequiresUserGesture(false);
+        wv.setMixedContentMode(wv.MIXED_CONTENT_ALWAYS_ALLOW);
+        wv.setUserAgentString(uaString);
     }
 
 
